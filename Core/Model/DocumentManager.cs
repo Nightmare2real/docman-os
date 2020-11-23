2fc61b110267f4f0fd5d050e314f219addb49b13
s103451 <s103451@student.dtu.dk>
2020-11-23 16:02:56 +0100

    Added generics Get method on FileSystemObject

--- Core/Model/DocumentManager.cs
+++ Core/Model/DocumentManager.cs
@@ -5,6 +5,7 @@ namespace Model
     public static class DocumentManager
     {
         public static List<Folder> Folders { get; } = new List<Folder>();
+        public static List<Document> Document { get; } = new List<Document>();
 
         public static DocumentContext OpenContext()=>
             new DocumentContext();
--- Core/Model/FileSystemObject.cs
+++ Core/Model/FileSystemObject.cs
@@ -11,5 +11,7 @@ namespace Model
         {
             get;set;
         }
+
+        public T Get<T>(string key) => (T)_fields[key];
     }
 }
